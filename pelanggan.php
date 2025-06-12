d233cb0deeef310b090da9babbb59bfc3e9b0092
mona2401010156 <yaniarimona@gmail.com>
2025-06-12 17:21:57 +0800

    Add files via upload

--- pelanggan.php
+++ pelanggan.php
@@ -60,7 +60,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="./index.php" class="nav-link active">
+            <a href="./index.php" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
@@ -68,10 +68,10 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="./pelanggan.php" class="nav-link ">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+            <a href="./pelanggan.php" class="nav-link active">
+              <i class="nav-icon fas fa-user"></i>
               <p>
-                Pelanggan
+                pelanggan
               </p>
             </a>
           </li>
@@ -106,68 +106,19 @@
     <section class="content">
       <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-              <div class="inner">
-                <h3>150</h3>
-
-                <p>New Orders</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-bag"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                <p>Bounce Rate</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h3>44</h3>
-
-                <p>User Registrations</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h3>65</h3>
-                <p>Unique Visitors</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-        </div>
-
-        <!-- ISI KONTEN DASHBAORD -->
+           <!-- konten utama -->
+       <div class="row">
+         <table id = "example1" class= "table table-border table-striped">
+          <tr>
+            <th> No.</th>
+            <th> Nama Pelanggan </th>
+            <th> No id </th>
+            <th> Alamat </th>
+            <th> No tlpn</th>
+            <th> OPSI </th>
+          </tr>
+         </table>
+          
 
       </div><!-- /.container-fluid -->
     </section>
